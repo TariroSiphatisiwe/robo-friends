2601cb715f6283683a58432f968e3c44a0aa806a
TariroSiphatisiwe <stariro419@gmail.com>
2022-06-08 15:54:51 +0200

    Changing classes to hooks

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import CardList from "./CardList";
 import SearchBox from './SearchBox';
 import './App.css';
 import Scroll from './Scroll';
 import ErrorBoundary from "./ErrorBoundary";
 
-class App extends React.Component{
-    constructor(){
+function App(){
+   /* constructor(){
     super()
     this.state={
         robots:[],
         searchfield:'',
     }
-    }
-    componentDidMount(){
+    }*/
+    useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response=>response.json())
         .then(users=>this.setState({robots:users}))
-    }
+    },[])
     
-    onsetchange=(event)=>{
-        this.setState({searchfield:event.target.value})
+    const [robots,setRobots]=useState([])
+    const [searchfield,setSearchField]=useState('')
+
+    const onsetchange=(event)=>{
+     setSearchField(event.target.value)
         }
 
-        render(){
-        const filterRobo=(this.state.robots.filter(robots=>{
-        return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+      
+        const filterRobo=(robots.filter(robots=>{
+        return robots.name.toLowerCase().includes(searchfield.toLowerCase());
      }))
-     if(this.state.robots.length===0){
+     if(robots.length===0){
         return <h1>Loading....</h1>;
     } else{
        return(
         <>
         <div className='tc'>
         <h1>Robo Friends</h1>
-        <SearchBox setchange={this.onsetchange}/>
+        <SearchBox setchange={onsetchange}/>
          <Scroll>
              <ErrorBoundary>
          <CardList robots={filterRobo}/>
@@ -43,7 +46,7 @@ class App extends React.Component{
          </div>
         </>
     )
-}
+
         }
 }
 export default App;
